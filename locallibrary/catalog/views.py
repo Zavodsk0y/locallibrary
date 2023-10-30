51de0db25956d99ae36fd32f42c6e8c1ccf96e93
Zavodsk0y <zimniyvolk5@gmail.com>
2023-10-30 11:37:49 +0700

    Завершена часть 5. Создано первое представление index. В котором описан функционал подсчета книг, копий, доступных копий, авторов, также добавлена фильтрация по содержанию какого-либо слова в названии книги, жанра (p.s. из-за sqlite и кодировки ASCII? данная функция сломана с кириллицей). Создана директория шаблонов templates, в котором создали базовый для всех последующих шаблон base_generic.html. Создали главную страницу index.html

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    # Генерация "количеств" некоторых главных объектов
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    # Доступные книги (статус = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors = Author.objects.count()  # Метод 'all()' применён по умолчанию.
+    num_roman_books = Book.objects.filter(title__icontains='нашего').count()
+    num_scifi_genres = Genre.objects.filter(name__icontains='Фантастика').count()
+
+
+
+
+    # Отрисовка HTML-шаблона index.html с данными внутри
+    # переменной контекста context
+    return render(
+        request,
+        'index.html',
+        context={'num_books': num_books, 'num_instances': num_instances,
+                 'num_instances_available': num_instances_available, 'num_authors': num_authors,
+                 'num_roman_books': num_roman_books, 'num_scifi_genres': num_scifi_genres},
+    )
